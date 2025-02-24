4f0cfb5b256bf4639c82095429449e3e2a730bd0
NN-Bayat <noorullahbayat1414@gmail.com>
2025-02-24 09:59:19 +0430

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     <>
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
       <div
-        className={`min-h-screen transition-opacity duration-700 ${
+        className={`min-h-screen flex flex-col justify-center transition-opacity duration-700 ${
           isLoaded ? "opacity-100" : "opacity-0"
         } bg-black text-gray-100`}
       >
