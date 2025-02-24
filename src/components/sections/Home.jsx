1d0d4cb8c137a0a4ff3e188b8f2f3fc77d0e21a6
NN-Bayat <noorullahbayat1414@gmail.com>
2025-02-24 10:01:19 +0430

    second commit

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -20,10 +20,10 @@ export const Home = () => {
           application. my goal is to build solution that offer both exceptional
           performance and a delightful user experience.
         </p>
-        <div className="flex justify-center space-x-4">
+        <div className="flex flex-wrap gap-6 justify-center">
           <a
             href="#projects"
-            className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition 
+            className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition whitespace-nowrap 
             relative overflow-hidden hover:-translate-y-[2px] hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
           >
             View Projects
@@ -31,7 +31,7 @@ export const Home = () => {
 
           <a
             href="#contact"
-            className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-[2px] hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-blue-500/10"
+            className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-[2px] hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-blue-500/10 whitespace-nowrap"
           >
             Contact Me
           </a>
