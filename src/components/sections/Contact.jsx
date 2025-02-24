6ca18b5dcdad1e23afa8f9dafcac6f6fb7240075
NN-Bayat <noorullahbayat1414@gmail.com>
2025-02-24 10:00:46 +0430

    second commit

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -7,7 +7,7 @@ export const Contact = () => {
       className="min-h-screen flex items-center justify-center py-20"
     >
       <RevealOnScroll>
-        <div className="px-4 w-150">
+        <div className="px-4 sm:w-150 w-screen">
           <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
             {" "}
             Get In Touch
@@ -19,7 +19,7 @@ export const Contact = () => {
                 id="name"
                 name="name"
                 required
-                className=" w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition 
+                className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition 
                  focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
                 placeholder="Name..."
               />
@@ -42,12 +42,20 @@ export const Contact = () => {
                 id="message"
                 name="message"
                 required
-                rows={5}
+                rows={7}
                 className=" w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition 
-                 focus:outline-none focus:border-blue-500 focus:bg-blue-500/5 resize-none"
+                 focus:outline-none focus:border-blue-500 focus:bg-blue-500/5 max-h-36 md:max-h-none"
                 placeholder="Your Message..."
               />
             </div>
+
+            <button
+              type="submit"
+              className="w-full bg-blue-500 text-white py-3 px-6 rounded font-medium transition 
+               relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]"
+            >
+              Send Message
+            </button>
           </form>
         </div>
       </RevealOnScroll>
